89a5bdba896810e8ed0c76551651bf227ee0aeab
Isis Parra <isiscapano@gmail.com>
2020-09-13 02:30:58 -0300

    app-apirest-toti

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require ('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(require('./routes/routes'));
+
+app.listen(3000);
+console.log('Servidor activo na porta 3000')
